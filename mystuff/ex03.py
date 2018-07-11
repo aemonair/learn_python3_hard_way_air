5184c37ecc40f0bac69fd41a3016d9f816e11eb9
Aemonair <v.aemonair@gmail.com>
2018-07-11 18:01:09 +0800

    ex03

--- mystuff/ex03.py
+++ mystuff/ex03.py
@@ -1,26 +1,27 @@
 #something for thinking
-print "I will now count my chickens"
+print ("I will now count my chickens")
 
-#print string,number
-print "Hens",25 +30/6
-print "Roosters",100 - 25 * 3 % 4
+#print( string,number
+print ("Hens",25 +30/6)
+print ("Roosters",100 - 25 * 3 % 4)
 
-print "Now I will count the eggs:"
-print 3 + 2 + 1 -5 + 4 % 2 -1 / 4 + 6
+print ("Now I will count the eggs:")
 
-print "Is it true that 3 + 2 < 5 - 7?"
+print (3 + 2 + 1 -5 + 4 % 2 -1 / 4 + 6)
 
-print 3 + 2 < 5 -7
-print 3/2,  5/7
-print 3.0/2,  5.0/7
+print ("Is it true that 3 + 2 < 5 - 7?")
 
-print "What is 3 + 2?",3 + 2
-print "What is 5 - 7?",5 - 7
+print (3 + 2 < 5 -7)
+print (3/2,  5/7)
+print (3.0/2,  5.0/7)
 
-print "Oh, that's why it's False."
+print ("What is 3 + 2?",3 + 2)
+print ("What is 5 - 7?",5 - 7)
 
-print "How about some more."
+print ("Oh, that's why it's False.")
 
-print "Is it greater?", 5 > -2
-print "Is it greater or equal?", 5 >=-2
-print "Is it less or equal?", 5 <= -2
+print ("How about some more.")
+
+print ("Is it greater?", 5 > -2)
+print ("Is it greater or equal?", 5 >=-2)
+print ("Is it less or equal?", 5 <= -2)
